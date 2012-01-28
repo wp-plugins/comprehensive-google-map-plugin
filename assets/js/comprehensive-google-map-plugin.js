cc2632155026bd87402750b951d80ecf120840c5
alexanderzagniotov <alexanderzagniotov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-28 03:31:56 +0000

    Version 5.0.0 trunk and tag
    git-svn-id: https://plugins.svn.wordpress.org/comprehensive-google-map-plugin/tags/5.0.0@496380 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/comprehensive-google-map-plugin.js
+++ assets/js/comprehensive-google-map-plugin.js
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+
 function generateWidgetShortcode(container_id) {
 	var id = '#' + container_id;
 	var code = buildShortcode(id);
@@ -41,6 +43,12 @@ function buildShortcode(id) {
 				}
 
 				if (role != null && role != "" && val != null && val != "") {
+
+					if (role.indexOf("_") > 0) {
+						role = role.replace(/_/g,"");
+					} if (role.indexOf("hidden") > 0) {
+						role = role.replace(/hidden/g,"");
+					}		
 					code += role + "=" + "\"" + val + "\" ";
 				}
 	});
@@ -48,6 +56,8 @@ function buildShortcode(id) {
 	code += "]";
 	return code;
 }
+
+
 
 function configureSlider(min, max, step, elem) {
 
@@ -72,31 +82,239 @@ function configureSlider(min, max, step, elem) {
 		change: function(event, ui) {
 			var sibling = jQuery(this).siblings("input[role=" + elem + "]");
 			var siblingId = jQuery(sibling).attr("id");
-			jQuery("input#" + siblingId).attr("value", ui.value);
+			var value =  ui.value;
+			if (!isNumber(step)) {
+				value =  ui.value.toFixed(5);
+			}
+			jQuery("input#" + siblingId).attr("value", value);
 		},
 
 	 	slide: function(event, ui) {
 			var sibling = jQuery(this).siblings("input[role=" + elem + "]");
 			var siblingId = jQuery(sibling).attr("id");
-			jQuery("input#" + siblingId).attr("value", ui.value);
+			var value =  ui.value;
+			if (!isNumber(step)) {
+				value =  ui.value.toFixed(5);
+			}
+			jQuery("input#" + siblingId).attr("value", value);
 	}});
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function initSliders() {
 	configureSlider(0, 20, 1, "zoom");
-   	configureSlider(0, 90, 1, "latitude");
-	configureSlider(-180, 180, 1, "longitude");
+   	configureSlider(-90, 90, 0.00001, "latitude");
+	configureSlider(-180, 180, 0.00001, "longitude");
 	configureSlider(60, 990, 5, "width");
 	configureSlider(60, 990, 5, "height");
 }
 
+function initTooltips()  {
+
+	jQuery('a.google-map-tooltip-marker').hover(function() {
+
+		var tooltip_marker_id = jQuery(this).attr('id');
+
+		jQuery("a#" + tooltip_marker_id + "[title]").tooltip({
+			effect: 'slide',
+			opacity: 0.8,
+			tipClass : "google-map-tooltip",
+			offset: [-5, 0],
+			events: {
+  				def: "click, mouseleave"
+			}
+		});
+
+		jQuery("a#" + tooltip_marker_id).mouseout(function(event) {
+			if (jQuery(this).data('tooltip')) {
+  				jQuery(this).data('tooltip').hide();
+			}
+		});
+	});
+}
+
+
+jQuery.TokenListHolder = function (classPath) {
+    jQuery.extend(this, jQuery.TokenListHolder.defaultOptions);
+
+	var lists = [];
+	jQuery.map(jQuery(classPath), function(element) {
+		var id = jQuery(element).attr("id");
+
+		if (id != null) {
+			lists.push({id : id, obj: jQuery(element).tokenInput({holderId: id})});
+		}
+	});
+
+	this.printLists = function() {
+		//console.log(lists);
+	}
+
+	this.getLists = function() {
+		return lists;
+	}
+
+
+	this.getListIds = function() {
+		var found = [];
+		jQuery.map(jQuery(lists), function(element) {
+			found.push(element.id);
+		});
+		return found;
+	}
+
+
+	this.getList = function(listId) {
+		var found = {};
+		jQuery.map(jQuery(lists), function(element) {
+			if (element.id == listId) {
+				found = element.obj;
+			}
+		});
+		return found;
+	}
+}
+
+function fadeInOutOnError(targetInput)  {
+
+		jQuery(targetInput).fadeIn("slow", function() {
+			jQuery(this).addClass("errorToken");
+		});
+
+     	jQuery(targetInput).focus().fadeOut(function() {
+			jQuery(this).removeClass("errorToken");
+			jQuery(this).fadeIn("slow");
+		});
+
+}
+
+function initTokenHoldersAndEvent()  {
+	var holder = new jQuery.TokenListHolder("div#widgets-right  ul.token-input-list, div#google-map-container-metabox ul.token-input-list");
+	var initLists = holder.getLists();
+
+	//console.log(initLists);
+
+		jQuery.map(jQuery(initLists), function(element) {
+			var hiddenInput = "#" + element.id + "hidden";
+			var csv = jQuery(hiddenInput).val();
+
+			if (csv != null && csv != "") {
+				var holderList = element.obj;
+				var locations = csv.split("|");
+				jQuery.map(locations, function (element) {
+        			holderList.add(element);
+				});
+			}
+		});
+
+	jQuery("input.add-additonal-location").click(function (source) {
+
+				var listId = jQuery(this).attr("id") + "list";
+				var tokenList = holder.getList(listId);
+				var targetInput = "#" + jQuery(this).attr("id") + "input";
+
+				var customIconListId = "#" + jQuery(this).attr("id") + "icons";
+				var selectedIcon = jQuery(customIconListId + " input[name='custom-icons-radio']:checked").val();
+
+				if (jQuery(targetInput).val() != null && jQuery(targetInput).val() != "") {
+					var target = jQuery(targetInput).val().replace(/^\s+|\s+$/g, '');
+//http://stackoverflow.com/questions/1997616/regex-for-password-that-requires-one-numeric-or-one-non-alphanumeric-character
+					var chars = /^(?=.*(\d|[a-zA-Z])).{5,}$/;
+					var hasValidChars = chars.test(target);
+					if (hasValidChars) {
+
+						//console.log(target + CGMPGlobal.sep + selectedIcon);
+						tokenList.add(target + CGMPGlobal.sep + selectedIcon);
+
+						resetPreviousIconSelection(jQuery(customIconListId));
+
+						jQuery(customIconListId + " img#default-marker-icon").attr("style", "cursor: default; ");
+						jQuery(customIconListId + " img#default-marker-icon").addClass('selected-marker-image');
+						jQuery(customIconListId + " input#default-marker-icon-radio").attr('checked', 'checked');
+
+						jQuery(targetInput).attr("style", "");
+						jQuery(targetInput).addClass("default-marker-icon");
+						jQuery(targetInput).val("");
+						jQuery(targetInput).focus();
+
+
+					} else {
+						fadeInOutOnError(targetInput);
+					}
+				} else {
+					fadeInOutOnError(targetInput);
+				}
+
+            	return false;
+    });
+}
+
+function resetPreviousIconSelection(parentDiv)  {
+	
+		jQuery.each(parentDiv.children(), function() {
+   			var liImg = jQuery(this).find("img");
+
+			if (liImg != null) {
+				jQuery(liImg).attr("style", "");
+				jQuery(liImg).removeClass('selected-marker-image');
+			}
+		});
+}
+
+function doMarkerIconUpdateOnSelection(parentDiv, img)  {
+
+	jQuery(img).attr("style", "cursor: default; ");
+	jQuery(img).addClass('selected-marker-image');
+
+	var currentSrc = jQuery(img).attr('src');
+	var inputId = jQuery(parentDiv).attr("id").replace("icons", "input");
+	jQuery("#" + inputId).attr("style", "background: url('" + currentSrc + "') no-repeat scroll 0px 0px #F9F9F9 !important");
+	jQuery("#" + inputId).removeClass("default-marker-icon");
+	jQuery("#" + inputId).focus();
+
+}
+
+function initMarkerIconEvents() {
+	//jQuery('img.nomarker').attr("style", "cursor: default; background-color: green; border-radius: 3px 3px 3px 3px;");
+
+	jQuery("div.custom-icons-placeholder a img").click(function () {
+		
+			var currentSrc = jQuery(this).attr('src');
+			if (currentSrc != null) {
+
+					var parentDiv = jQuery(this).closest("div.custom-icons-placeholder");
+					resetPreviousIconSelection(parentDiv);
+
+					jQuery(this).parent("a").siblings('input[name="custom-icons-radio"]').attr("checked", "checked");
+					doMarkerIconUpdateOnSelection(parentDiv, jQuery(this));
+			}
+	});
+
+
+	jQuery("input[name='custom-icons-radio']").click(function () {
+
+			var img = jQuery(this).siblings("a").children('img');
+			var currentSrc = jQuery(img).attr('src');
+			if (currentSrc != null) {
+
+					var parentDiv = jQuery(this).closest("div.custom-icons-placeholder");
+					resetPreviousIconSelection(parentDiv);
+
+					doMarkerIconUpdateOnSelection(parentDiv, img);
+			}
+	});
+
+}
+
 jQuery(document).ready(function() {
 
-	
+	initTokenHoldersAndEvent();
 	initSliders(); 
-
-	jQuery("div#widgets-right .widget-google-map-container a[title]").tooltip({effect : "fade", opacity: 0.8});
-	jQuery("#google-map-container-metabox a[title]").tooltip({effect : "fade", opacity: 0.8});
+	initTooltips();
+	initMarkerIconEvents();
 
 	jQuery("ul.tools-tabs-nav").tabs("div.tools-tab-body", {
         tabs: 'li',
@@ -108,19 +326,21 @@ jQuery(document).ajaxSuccess(
 	function (e, x, o) {
     	jQuery(document).ready(
     		function ($) {
-				var indexOf = o.data.indexOf('id_base=comprehensivegooglemap');
+				
+				if (o.data != null)	{
+				
+					var indexOf = o.data.indexOf('id_base=comprehensivegooglemap');
 
-				if (indexOf > 0) {
-					initSliders();
-					
-					//console.log(jQuery("div#widgets-right .widget-google-map-container"));
-					jQuery("div#widgets-right .widget-google-map-container a[title]").tooltip({effect : "fade", opacity: 0.8});
-					jQuery("#google-map-container-metabox a[title]").tooltip({effect : "fade", opacity: 0.8});
+					if (indexOf > 0) {
+						initTokenHoldersAndEvent();
+						initSliders();
+						initTooltips();
+						initMarkerIconEvents();
+					}
 				}
 			}
 		);
 	}
 );
-
 
 
--- shortcode.php
+++ shortcode.php
@@ -24,8 +24,8 @@ function cgmp_shortcode_googlemap_handler($attr, $content = null, $code = null) 
 		'width' => 250,
 		'height' => 250,
 		'zoom' => 5,
-		'latitude' => 40.69847032728747,
-		'longitude' => -73.9514422416687,
+		'latitude' => 0,
+		'longitude' => 0,
 		'zoom' => 5,
 		'width' => 400,
 		'height' => 400,
@@ -34,36 +34,84 @@ function cgmp_shortcode_googlemap_handler($attr, $content = null, $code = null) 
 		'infobubblecontent' => '',
 		'animation' => 'DROP',
 		'm_aptypecontrol' => 'true',
+		'maptypecontrol' => 'true',
 		'pancontrol' => 'true',
 		'z_oomcontrol' => 'true',
+		'zoomcontrol' => 'true',
 		'scalecontrol' => 'true',
 		'streetviewcontrol' => 'true',
 		'addresscontent' => '',
 		'showbike' => 'false',
+		'bubbleautopan' => 'nada',
 		'showtraffic' => 'false',
-		'kml' => ''
+		'showpanoramio' => 'false',
+		'addmarkerlist' => '',
+		'kml' => '',
+		'markerdirections' => 'true',
+		'mapalign' => 'center',
+		'panoramiouid' => ''
 	), $attr));
 	
-
 	$id = md5(time().' '.rand()); 
 
+	if ($bubbleautopan == 'nada') {
+		$bubbleautopan = 'false';
+	} 
+
+	
 	$controlOpts = array();
-	$controlOpts['m_aptypecontrol'] = $m_aptypecontrol;
+
+	if ($m_aptypecontrol == "true" && $maptypecontrol == "false") {
+			$controlOpts['m_aptypecontrol'] = "false";
+
+	} else 	if ($m_aptypecontrol == "false" && $maptypecontrol == "true") {
+			$controlOpts['m_aptypecontrol'] = "false";
+
+	} else {
+			$controlOpts['m_aptypecontrol'] = "true";
+	} 
+
 	$controlOpts['pancontrol'] = $pancontrol;
-	$controlOpts['z_oomcontrol'] = $z_oomcontrol;
+	
+	if ($z_oomcontrol == "true" && $zoomcontrol == "false") {
+			$controlOpts['z_oomcontrol'] = "false";
+
+	} else 	if ($z_oomcontrol == "false" && $zoomcontrol == "true") {
+			$controlOpts['z_oomcontrol'] = "false";
+
+	} else {
+			$controlOpts['z_oomcontrol'] = "true";
+	} 
+
+	
 	$controlOpts['scalecontrol'] = $scalecontrol;
 	$controlOpts['streetviewcontrol'] = $streetviewcontrol;
 
+	$legacyLoc = isset($addresscontent) ? $addresscontent : "";
+
+	if (isset($latitude) && isset($longitude)) {
+		if ($latitude != "0" && $longitude != "0" && $latitude != 0 && $longitude != 0) {
+			$legacyLoc = $latitude.",".$longitude;
+		}
+	}
+
+	if (trim($legacyLoc) != "")  {
+		$addmarkerlist = $legacyLoc.CGMP_SEP."1-default.png"."|".$addmarkerlist;
+	}
+
+
 	$result = '';
-	$result .= cgmp_draw_map_placeholder($id, $width, $height);
-	$result .= cgmp_begin_map_init($id, $latitude, $longitude, $zoom, $maptype, $controlOpts);
-	$result .= cgmp_draw_map_marker($id, $showmarker, $animation);
-	$result .= cgmp_draw_marker_infobubble($id, $infobubblecontent);
-	$result .= cgmp_draw_map_address($id, $addresscontent);
+	$result .= cgmp_draw_map_placeholder($id, $width, $height, $markerdirections, $mapalign);
+	$result .= cgmp_begin_map_init($id, $latitude, $longitude, $zoom, $maptype, $bubbleautopan, $controlOpts, $markerdirections);
+	//$result .= cgmp_draw_map_marker($id, $showmarker, $animation, $addresscontent, $addmarkerlist, $kml, $latitude, $longitude);
+	$result .= cgmp_draw_map_marker_v2($id, $addmarkerlist, $kml);
 	$result .= cgmp_draw_map_bikepath($id, $showbike);
 	$result .= cgmp_draw_map_traffic($id, $showtraffic);
+	$result .= cgmp_draw_panoramio($id, $showpanoramio, $panoramiouid);
 	$result .= cgmp_draw_kml($id, $kml);
 	$result .= cgmp_end_map_init();
+
+
 	return $result;
 }
 endif;
