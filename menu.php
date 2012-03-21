7e502ca86d7399896eb3f5528120b9ea8a17df98
alexanderzagniotov <alexanderzagniotov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-21 09:23:57 +0000

    Version 7.0.20
    git-svn-id: https://plugins.svn.wordpress.org/comprehensive-google-map-plugin/tags/7.0.20@521721 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu.php
+++ menu.php
@@ -18,9 +18,59 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 if ( !function_exists('cgmp_google_map_plugin_menu') ):
       function cgmp_google_map_plugin_menu() {
-            add_menu_page("Comprehensive Google Map", 'Google Map', 'activate_plugins', basename(__FILE__), 'cgmp_parse_menu_html', CGMP_PLUGIN_IMAGES .'/google_map.png');
-      }
+      		$hook = add_menu_page("Comprehensive Google Map", 'Google Map', 'activate_plugins', CGMP_HOOK, 'cgmp_parse_menu_html', CGMP_PLUGIN_IMAGES .'/google_map.png');
+	  		add_action('admin_print_scripts-'.$hook, 'cgmp_google_map_tab_script');
+			$hook = add_submenu_page(CGMP_HOOK, 'Shortcode Builder', 'Shortcode Builder', 'activate_plugins', 'cgmp-shortcodebuilder', 'cgmp_shortcodebuilder_callback' );
+			add_action('admin_print_scripts-'.$hook, 'cgmp_google_map_tab_script');
+			$hook = add_submenu_page(CGMP_HOOK, 'Settings', 'Settings', 'activate_plugins', 'cgmp-settings', 'cgmp_settings_callback' );
+		   	add_action('admin_print_scripts-'.$hook, 'cgmp_google_map_tab_script');
+	  }
 endif;
+
+if ( !function_exists('cgmp_settings_callback') ):
+
+	function cgmp_settings_callback() {
+
+		if (!current_user_can('activate_plugins'))  {
+             	wp_die( __('You do not have sufficient permissions to access this page.') );
+		}
+
+		if (isset($_POST['cgmp-save-settings']))  {
+				update_option(CGMP_DB_SETTINGS_BUILDER_LOCATION, $_POST['builder-under-post']);
+				cgmp_show_message("Settings updated successfully!");
+		}
+
+		$setting_builder_location = get_option(CGMP_DB_SETTINGS_BUILDER_LOCATION);
+
+		$yes_display_radio_btn = "";
+		$no_display_radio_btn = "checked='checked'";
+		if (isset($setting_builder_location) && $setting_builder_location == "true") {
+			$no_display_radio_btn = "";
+			$yes_display_radio_btn = "checked='checked'";
+		}
+
+		$template_values = array();
+        $template_values["YES_DISPLAY_SHORTCODE_BUILDER_INPOST_TOKEN"] = $yes_display_radio_btn;
+		$template_values["NO_DISPLAY_SHORTCODE_BUILDER_INPOST_TOKEN"] = $no_display_radio_btn;
+        echo cgmp_render_template_with_values($template_values, CGMP_HTML_TEMPLATE_PLUGIN_SETTINGS_PAGE);
+	}
+
+endif;
+
+
+if ( !function_exists('cgmp_shortcodebuilder_callback') ):
+
+	function cgmp_shortcodebuilder_callback() {
+
+		if (!current_user_can('activate_plugins'))  {
+             	wp_die( __('You do not have sufficient permissions to access this page.') );
+        }
+
+		include_once(CGMP_PLUGIN_INCLUDE_DIR.'/shortcode_builder_form.php');
+		echo cgmp_render_template_with_values(array("SHORTCODEBUILDER_TOKEN" => $map_configuration_template), CGMP_HTML_TEMPLATE_MAP_SHORTCODE_BUILDER_PAGE);
+	}
+endif;
+
 
 if ( !function_exists('cgmp_parse_menu_html') ):
 function cgmp_parse_menu_html() {
@@ -28,63 +78,16 @@ function cgmp_parse_menu_html() {
                 wp_die( __('You do not have sufficient permissions to access this page.') );
         }
 
-	$template = file_get_contents(CGMP_PLUGIN_HTML."/documentation.plug");
-        $template_content = file_get_contents(CGMP_PLUGIN_HTML."/form_body_template.plug");
+		$json_html_doco_params = cgmp_fetch_json_data_file(CGMP_JSON_DATA_HTML_ELEMENTS_DOCO_PARAMS);
 
-        $template_values = array();
-        $template_values["LABEL_WIDTH"] = "<b>Width</b>:";
-        $template_values["INPUT_WIDTH"] = "The width of the map placeholder DIV in pixels";
-        $template_values["LABEL_HEIGHT"] = "<b>Height</b>:";
-        $template_values["INPUT_HEIGHT"] = "The height of the map placeholder DIV in pixels";
-        $template_values["LABEL_LATITUDE"] = "<b>Latitude</b>:";
-        $template_values["INPUT_LATITUDE"] = "Together with Longitude, makes a geographic coordinate of a location displayed on the Google map. The latitude coordinate value is measured in degrees";
-        $template_values["LABEL_LONGITUDE"] = "<b>Longitude</b>:";
-        $template_values["INPUT_LONGITUDE"] =  "Together with Latitude, makes a geographic coordinate of a location displayed on the Google map. The longitude coordinate value is measured in degrees";
-        $template_values["LABEL_ZOOM"] = "<b>Zoom</b>:";
-        $template_values["INPUT_ZOOM"] = "Each map also contains a zoom level, which defines the resolution of the current view. Zoom levels between 0 (the lowest zoom level, in which the entire world can be seen on one map) to 19 (the highest zoom level, down to individual buildings) are possible within the normal maps view. Zoom levels vary depending on where in the world you're looking, as data in some parts of the globe is more defined than in others. Zoom levels up to 20 are possible within satellite view.";
-        $template_values["LABEL_MAPTYPE"] = "<b>Map type</b>:";
-        $template_values["SELECT_MAPTYPE"] = "There are many types of maps available within the Google Maps. In addition to the familiar 'painted' road map tiles, the Google Maps API also supports other maps types. The following map types are available in the Google Maps API:
-ROADMAP displays the default road map view, SATELLITE displays Google Earth satellite images, HYBRID displays a mixture of normal and satellite views, TERRAIN displays a physical map based on terrain information.";
-        
-        $template_values["LABEL_SHOWMARKER"] = "<b>Marker</b>";
-        $template_values["SELECT_SHOWMARKER"] = "If a map is specified, the marker is added to the map upon construction. Note that the position must be set for the marker to display."; 
-        $template_values["LABEL_ANIMATION"] = "<b>Animation</b>";
-        $template_values["SELECT_ANIMATION"]    = "Animations can be played on a marker. Currently two types of animations supported: BOUNCE makes marker to bounce until animation is stopped, DROP makes marker to fall from the top of the map ending with a small bounce.";
-        $template_values["LABEL_M_APTYPECONTROL"] = "<b>MapType</b>";
-        $template_values["SELECT_M_APTYPECONTROL"] = "The MapType control lets the user toggle between map types (such as ROADMAP and SATELLITE). This control appears by default in the top right corner of the map";
-	$template_values["LABEL_PANCONTROL"] = "<b>Pan</b>";
-        $template_values["SELECT_PANCONTROL"] = "The Pan control displays buttons for panning the map. This control appears by default in the top left corner of the map on non-touch devices. The Pan control also allows you to rotate 45° imagery, if available.";
-        $template_values["LABEL_Z_OOMCONTROL"] = "<b>Zoom</b>";
-        $template_values["SELECT_Z_OOMCONTROL"] = "The Zoom control displays a slider (for large maps) or small '+/-' buttons (for small maps) to control the zoom level of the map. This control appears by default in the top left corner of the map on non-touch devices or in the bottom left corner on touch devices.";
-        $template_values["LABEL_SCALECONTROL"] = "<b>Scale</b>"; 
-        $template_values["SELECT_SCALECONTROL"] = "The Scale control displays a map scale element. This control is not enabled by default.";
-        $template_values["LABEL_STREETVIEWCONTROL"] = "<b>StreetView</b>";
-        $template_values["SELECT_STREETVIEWCONTROL"] = "The Street View control contains a Pegman icon which can be dragged onto the map to enable Street View. This control appears by default in the top left corner of the map";
+		if (is_array($json_html_doco_params)) {
+			$map_configuration_form_template = cgmp_render_template_with_values($json_html_doco_params, CGMP_HTML_TEMPLATE_MAP_CONFIGURATION_FORM);
+			$template_values = array();
+        	$template_values["DOCUMENTATION_TOKEN"] = $map_configuration_form_template;
 
-        $template_values["LABEL_INFOBUBBLECONTENT"] = "<b>Content Text</b>"; 
-        $template_values["INPUT_INFOBUBBLECONTENT"] = "Text to be displayed inside info bubble (info window).";
-
-        $template_values["LABEL_ADDRESSCONTENT"] = "<b>Address Text</b>"; 
-        $template_values["INPUT_ADDRESSCONTENT"] = "Geographical gestination address string. The address supersedes longitude and latitude configuration. If the address provided cannot be parsed (eg: invalid address) by Google, the map will display error message in the info bubble over default location (New York, USA). Please note, address configuration *supersedes* latitude/longitude settings";
-
-        $template_values["LABEL_SHOWBIKE"] = "<b>Bike Paths</b>";
-        $template_values["SELECT_SHOWBIKE"] = "A layer showing bike lanes and paths as overlays on a Google Map.";
-        $template_values["LABEL_SHOWTRAFFIC"] = "<b>Traffic Info</b>";
-        $template_values["SELECT_SHOWTRAFFIC"] = "A layer showing vehicle traffic as overlay on a Google Map.";
-        $template_values["LABEL_KML"] = "<b>KML/GeoRSS URL</b>";
-        $template_values["INPUT_KML"] = "KML is a file format used to display geographic data in an earth browser, such as Google Earth, Google Maps, and Google Maps for mobile. A KML file is processed in much the same way that HTML (and XML) files are processed by web browsers. Like HTML, KML has a tag-based structure with names and attributes used for specific display purposes. Thus, Google Earth and Maps act as browsers for KML files. Please note, KML configuration *supersedes* address and latitude/longitude settings";
-
-	global $global_fieldset_names;
-        $template_content = cgmp_replace_template_tokens($global_fieldset_names, $template_content);
-        $template_content = cgmp_replace_template_tokens($template_values, $template_content);
-
-        $template_values = array();
-        $template_values["DOCUMENTATION_TOKEN"] = $template_content;
-
-        $template = cgmp_replace_template_tokens($template_values, $template);
-        echo $template;
-
-}	
+        	echo cgmp_render_template_with_values($template_values, CGMP_HTML_TEMPLATE_MAP_CONFIG_DOCUMENTATION_PAGE);
+		}
+}
 endif;
 
 ?>
