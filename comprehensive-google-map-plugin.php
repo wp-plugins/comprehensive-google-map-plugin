78cabdb2d2e8a7c4a2c8cf43a2a16f5ddc2363f8
alexanderzagniotov <alexanderzagniotov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-10 06:45:22 +0000

    Tagging v8.0.4
    git-svn-id: https://plugins.svn.wordpress.org/comprehensive-google-map-plugin/tags/8.0.4@818056 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comprehensive-google-map-plugin.php
+++ comprehensive-google-map-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Comprehensive Google Map Plugin
 Plugin URI: http://initbinder.com/comprehensive-google-map-plugin
 Description: A simple and intuitive, yet elegant and fully documented Google map plugin that installs as a widget and a short code. The plugin is packed with useful features. Widget and shortcode enabled. Offers extensive configuration options for markers, over 250 custom marker icons, marker Geo mashup, controls, size, KML files, location by latitude/longitude, location by address, info window, directions, traffic/bike lanes and more. 
-Version: 8.0.3
+Version: 8.0.4
 Author: Alexander Zagniotov
 Author URI: http://initbinder.com
 License: GPLv2
